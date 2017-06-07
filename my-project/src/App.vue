ebff728fe87cf2633a31e4aa3eabe39cfaa7486f
wpf <851118434@qq.com>
2017-06-07 19:25:40 +0800

    add

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+  	<div class="hello">
+			<el-row :gutter="5">
+			  <el-col :span="20">
+			  	<div class="grid-content bg-purple">
+			  		<h1>主标题</h1>
+			  	</div>
+			  </el-col>
+			  
+			  <el-col :span="2">
+			  	<div class="grid-content bg-purple">
+			  			<button type="button" class="el-button el-button--default"><span><font><font class="">user</font></font></span></button>
+			  	</div>
+			  </el-col>
+			  <el-col :span="2">
+			  	<div class="grid-content bg-purple">
+				  	<i class="el-icon-arrow-down"></i>
+			  	</div>
+			  </el-col>
+			</el-row>
+	  </div>
     <router-view></router-view>
   </div>
 </template>
@@ -12,12 +31,22 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	.el-row {
+	    &:last-child {
+	      margin-bottom: 0;
+	    }
+	  }
+	  .el-col {
+	    border-radius: 4px;
+	  }
+	  .bg-purple {
+	    background: #20a0ff;
+	    text-align: center;
+	    color: white;
+	  }
+	  .grid-content {
+	    border-radius: 4px;
+	    min-height: 36px;
+	    line-height: 36px;
+	  }
 </style>
